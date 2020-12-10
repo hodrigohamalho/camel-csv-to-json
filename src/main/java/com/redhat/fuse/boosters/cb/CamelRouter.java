d5aae9b87db5463e0975d5e795d0a1a08ab7c820
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2020-12-10 15:56:38 -0300

    integrating with pam

--- src/main/java/com/redhat/fuse/boosters/cb/CamelRouter.java
+++ src/main/java/com/redhat/fuse/boosters/cb/CamelRouter.java
@@ -4,7 +4,6 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.spi.DataFormat;
 import org.springframework.stereotype.Component;
 
@@ -13,34 +12,24 @@ public class CamelRouter extends RouteBuilder {
 	
 	DataFormat bindy = new BindyCsvDataFormat(Person.class);
 
-
     @Override
     public void configure() throws Exception {
 
-        from("file:/tmp?fileName=file.csv")
-        	.streamCaching()
-            .log("OPA: ${body}")
-             .unmarshal(bindy)
-             .marshal()
-             .json(JsonLibrary.Jackson).log("${body}")
-             .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-             .to("http4://localhost:8080/camel/hello")
-             .to("file:/tmp/?fileName=file-in-json.json");
+        String kieServerUrl = "localhost:8080/kie-server/services/rest/server";
+        String containerId = "order-management";
+        String processId = "Order-Management.order-management";
 
-         restConfiguration()
-             .component("servlet")
-             .bindingMode(RestBindingMode.json);
+        from("file:/tmp?fileName=file.csv").streamCaching()
+            .unmarshal(bindy)
+            .marshal()
+            .json(JsonLibrary.Jackson).log("${body}")
+            .setHeader("authMethod", constant("Basic"))
+            .setHeader("authUsername", constant("kieserver"))
+            .setHeader("authPassword", constant("kiepass"))
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+            .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+            .to("http4://"+kieServerUrl +"/containers/"+ containerId + "/processes/ "+ processId +"/instances");
         
-         rest("/hello").description("Greetings REST service")
-             .consumes("application/json")
-             .produces("application/json")
-             
-             .post().outType(String.class)
-                 .responseMessage().code(200).endResponseMessage()
-                 .to("direct:impl");
-
-         from("direct:impl").description("REST service implementation route")
-         	.log("Body -> ${body}");
     }
 
 }
